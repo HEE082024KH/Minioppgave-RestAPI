0a34690a60494b7d21ad47ea309c226e2c1571b5
hee082024kh <hee082024@gmail.com>
2024-11-19 10:40:04 +0100

    Initial commit, added Model class

---
+++ Model.cs
@@ -0,0 +1,7 @@
+public class VideoGames
+{
+  public required int Id { get; set; }
+  public required string Title { get; set; }
+  public required string Studio { get; set; }
+  public required int Year { get; set; }
+}
